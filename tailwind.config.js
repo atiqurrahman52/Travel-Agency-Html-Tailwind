edd0d823d63671d2a413bbcb734041949463aabc
atiqurrahman52 <atiqur.rahman@intelli.global>
2022-09-10 15:16:37 +0600

    Navbar done

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  // content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        quentin: ["Quentin", "sans-serif"],
+        nunito:["Nunito", "sans-serif"],
+        squarepeg:["Square Peg", "cursive"],
+        nunitoSans:["Nunito Sans", "sans-serif"],
+        
+      },
+    },
+  },
+  plugins: [],
+}

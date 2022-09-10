4c4a7185934eb0dbf5ca6145d9d323c995cfb351
atiqurrahman52 <atiqur.rahman@intelli.global>
2022-09-10 16:47:13 +0600

    hero section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,24 @@ module.exports = {
   content: ["*"],
   // content: [],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "16px",
+        sm: "16px",
+        md: "20px",
+        lg: "24px",
+        xl: "40px",
+        "2xl": "60px",
+      },
+    },
     extend: {
       fontFamily: {
         quentin: ["Quentin", "sans-serif"],

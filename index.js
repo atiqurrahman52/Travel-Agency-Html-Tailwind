dc67ae4a841a7052bc2d02ac02b4d43da8841b20
atiqurrahman52 <atiqur.rahman@intelli.global>
2022-09-13 12:47:05 +0600

    europe slider done

--- index.js
+++ index.js
@@ -1,45 +1,50 @@
-// asia slider 
-$(document).ready(function() {
-  $('.asia').slick({
-    autoplay: true,
-    dots: false,
-    // arrows: true,
-    pauseOnHover: true,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    speed: 500,
-    centerMode: true,
-    centerPadding: '200px',
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          // slidesToScroll: 3,
-          // infinite: true,
-          // dots: true
-        }
-      },
-      {
-        breakpoint: 800,
-        settings: {
-          slidesToShow: 2,
-          // slidesToScroll: 2,
-          // initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          // slidesToScroll: 1
-        }
-      }
-    ]
-  });
-} )
+$('.asia').slick({
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay:true,
+  centerMode: true,
+  centerPadding: '200px',
+  prevArrow: false, nextArrow: false ,
 
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        centerPadding: '40px',
+      }
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 2,
+        centerMode: false,
+        centerPadding: '40px',
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        centerMode: false,
+        centerPadding: '40px',
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        centerMode:false,
+        centerPadding: '40px',
+        
+       
+      }
+    }
+  
+  ]
+});
 
 // partners slider 
 $(document).ready(function() {
@@ -75,3 +80,40 @@ $(document).ready(function() {
       });
 });
 
+
+// gallery 
+$('#gallery').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  // autoplaySpeed: 0,
+  // speed: 8000,
+  pauseOnHover: false,
+  infinite: true,
+      // slidesToShow: 3,
+      // slidesToScroll: 1,
+      // speed: 500,
+      centerMode: true,
+      centerPadding: '200px',
+  // cssEase: 'linear',
+  responsive: [
+                  {
+                  breakpoint:768,
+                  setting:{
+                      slidesToShow: 2,
+                  }
+              },{
+                  breakpoint:520,
+                  setting:{
+                      slidesToShow:1,
+                      // slidesToScroll :2,
+                  }
+              },
+              {
+                  breakpoint:364,
+                  setting:{
+                      slidesToShow:1,
+                  }
+              },
+          ]
+});

b9ba9131e4e4e904db2115827f535b0318e5b20f
atiqurrahman52 <atiqur.rahman@intelli.global>
2022-09-22 20:15:17 +0600

    footer updated

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// const activePage = window.location.pathname;
-// const navLinks = document.querySelectorAll('nav ul li a').
-// forEach(link =>{
-//     if(link.href.includes(`${activePage}`)){
-//         link.classList.add('active')
-//     }
-// })
 
 //  navbar 
 const button = document.querySelector('#menu-button');

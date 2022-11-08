fb11741ec27b2b2544ea7afbd44f87543fea712d
atiqurrahman52 <atiqur.rahman@intelli.global>
2022-11-08 10:54:19 +0600

    preloader done

--- index.js
+++ index.js
@@ -209,7 +209,17 @@ let accordions = document.querySelectorAll('.accordion');
 
 
 
-
+// preloader started
+document.onreadystatechange = function() {
+  if (document.readyState === "complete") {
+  $("#panel_left").addClass("panel_left");
+  $("#panel_right").addClass("panel_right");
+  $("#loader").addClass("loaded-circle");
+  $("#loader-img").addClass("loaded-img");
+  $("#preloader").addClass("loaded-img");
+ }
+ } 
+// preloader ended
 
 
       
